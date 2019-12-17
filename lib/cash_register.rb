ee84a14f88742897bdb170c327664ce0a8385109
David Cantoran <davidcantoran36@gmail.com>
2019-12-17 17:24:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
     self.total += price * quantity 
     quantity.times do   
       @items << title 
-      binding.pry
+      # binding.pry
     end 
      self.last_transaction = price
   end 
